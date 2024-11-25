ae0f9866b645db5d4c01cdcf7dc154ab78130558
walm <usernameforthis123@gmail.com>
2024-11-25 17:32:18 -0500

    Update vscode customizations

---
+++ Windows/vscode.js
@@ -0,0 +1,4 @@
+        let titlebarLeft = document.querySelector("div.titlebar-left");
+        let textElement = document.createElement("span");
+        textElement.textContent = "My Custom Text";
+        titlebarLeft.appendChild(textElement);
